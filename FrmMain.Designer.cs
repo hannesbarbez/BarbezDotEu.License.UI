bd206378347b15df830e3353110bae659aa4aa0f
Barbez.eu <hannesbarbez@hotmail.com>
2019-02-06 22:49:22 +0100

    Initial commit.

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Serial_Number_Generator
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.btnGenerateSerials = new System.Windows.Forms.Button();
+            this.tbSerials = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNumberOfSerials = new System.Windows.Forms.TextBox();
+            this.rbScreenshotOne = new System.Windows.Forms.RadioButton();
+            this.rbEisat = new System.Windows.Forms.RadioButton();
+            this.rbVocabulary = new System.Windows.Forms.RadioButton();
+            this.rbShutdown = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbDoNotInclude = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGenerateSerials
+            // 
+            this.btnGenerateSerials.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerateSerials.Location = new System.Drawing.Point(393, 12);
+            this.btnGenerateSerials.Name = "btnGenerateSerials";
+            this.btnGenerateSerials.Size = new System.Drawing.Size(119, 23);
+            this.btnGenerateSerials.TabIndex = 0;
+            this.btnGenerateSerials.Text = "Generate Serials";
+            this.btnGenerateSerials.UseVisualStyleBackColor = true;
+            this.btnGenerateSerials.Click += new System.EventHandler(this.btnGenerateSerialNumber_Click);
+            // 
+            // tbSerials
+            // 
+            this.tbSerials.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSerials.Location = new System.Drawing.Point(12, 86);
+            this.tbSerials.Multiline = true;
+            this.tbSerials.Name = "tbSerials";
+            this.tbSerials.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbSerials.Size = new System.Drawing.Size(500, 183);
+            this.tbSerials.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "# of serials to generate:";
+            // 
+            // tbNumberOfSerials
+            // 
+            this.tbNumberOfSerials.Location = new System.Drawing.Point(136, 14);
+            this.tbNumberOfSerials.MaxLength = 5;
+            this.tbNumberOfSerials.Name = "tbNumberOfSerials";
+            this.tbNumberOfSerials.Size = new System.Drawing.Size(44, 20);
+            this.tbNumberOfSerials.TabIndex = 3;
+            // 
+            // rbScreenshotOne
+            // 
+            this.rbScreenshotOne.AutoSize = true;
+            this.rbScreenshotOne.Location = new System.Drawing.Point(15, 40);
+            this.rbScreenshotOne.Name = "rbScreenshotOne";
+            this.rbScreenshotOne.Size = new System.Drawing.Size(121, 17);
+            this.rbScreenshotOne.TabIndex = 4;
+            this.rbScreenshotOne.Text = "Screenshot One Pro";
+            this.rbScreenshotOne.UseVisualStyleBackColor = true;
+            // 
+            // rbEisat
+            // 
+            this.rbEisat.AutoSize = true;
+            this.rbEisat.Location = new System.Drawing.Point(142, 40);
+            this.rbEisat.Name = "rbEisat";
+            this.rbEisat.Size = new System.Drawing.Size(227, 17);
+            this.rbEisat.TabIndex = 5;
+            this.rbEisat.Text = "Experience Index System Assessment Tool";
+            this.rbEisat.UseVisualStyleBackColor = true;
+            // 
+            // rbVocabulary
+            // 
+            this.rbVocabulary.AutoSize = true;
+            this.rbVocabulary.Location = new System.Drawing.Point(15, 63);
+            this.rbVocabulary.Name = "rbVocabulary";
+            this.rbVocabulary.Size = new System.Drawing.Size(90, 17);
+            this.rbVocabulary.TabIndex = 6;
+            this.rbVocabulary.Text = "Vocabulary++";
+            this.rbVocabulary.UseVisualStyleBackColor = true;
+            // 
+            // rbShutdown
+            // 
+            this.rbShutdown.AutoSize = true;
+            this.rbShutdown.Checked = true;
+            this.rbShutdown.Location = new System.Drawing.Point(142, 63);
+            this.rbShutdown.Name = "rbShutdown";
+            this.rbShutdown.Size = new System.Drawing.Size(73, 17);
+            this.rbShutdown.TabIndex = 7;
+            this.rbShutdown.TabStop = true;
+            this.rbShutdown.Text = "Shutdown";
+            this.rbShutdown.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Do not include:";
+            // 
+            // tbDoNotInclude
+            // 
+            this.tbDoNotInclude.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbDoNotInclude.Location = new System.Drawing.Point(12, 288);
+            this.tbDoNotInclude.MaxLength = 2048000;
+            this.tbDoNotInclude.Multiline = true;
+            this.tbDoNotInclude.Name = "tbDoNotInclude";
+            this.tbDoNotInclude.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbDoNotInclude.Size = new System.Drawing.Size(500, 137);
+            this.tbDoNotInclude.TabIndex = 9;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 437);
+            this.Controls.Add(this.tbDoNotInclude);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rbShutdown);
+            this.Controls.Add(this.rbVocabulary);
+            this.Controls.Add(this.rbEisat);
+            this.Controls.Add(this.rbScreenshotOne);
+            this.Controls.Add(this.tbNumberOfSerials);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSerials);
+            this.Controls.Add(this.btnGenerateSerials);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.Text = "MicroHann Serial Number Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerateSerials;
+        private System.Windows.Forms.TextBox tbSerials;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNumberOfSerials;
+        private System.Windows.Forms.RadioButton rbScreenshotOne;
+        private System.Windows.Forms.RadioButton rbEisat;
+        private System.Windows.Forms.RadioButton rbVocabulary;
+        private System.Windows.Forms.RadioButton rbShutdown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbDoNotInclude;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Serial_Number_Generator
+{
+    public partial class FrmMain : Form
+    {
+        private const int REQUIREDSMALLSERIALLENGTH = 5;
+        private const int RESULTINGSUM_SHUT = 70;
+        private const int RESULTINGSUM_SCRONEPRO = 68;
+        private const int RESULTINGSUM_EISAT = 66;
+        private const int RESULTINGSUM_VOCAB = 64;
+        private const string DIVIDER = "-";
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerateSerialNumber_Click(object sender, EventArgs e)
+        {
+            List<string> doNotInclude = new List<string>(tbDoNotInclude.Lines);
+            for (int i = 0; i < doNotInclude.Count; i++)
+                doNotInclude[i] = doNotInclude[i].Trim(new char[] { ' ', '\r', '\n' });
+
+            int amountOfSerialNumbers = 1;
+            int.TryParse(tbNumberOfSerials.Text, out amountOfSerialNumbers);
+            int reqBigSerialLength = REQUIREDSMALLSERIALLENGTH * 5;
+            tbSerials.Clear();
+            List<string> collection = new List<string>();
+
+            for (int i = 0; i < amountOfSerialNumbers; i++)
+            {
+                int[] seq1 = GetSequence();
+                int[] seq2 = GetSequence();
+                int[] seq3 = GetSequence();
+                int[] seq4 = GetSequence();
+                int[] seq5 = GetSequence();
+
+                string smallSerial1 = ((char)seq1[0]).ToString() + ((char)seq2[0]).ToString() + ((char)seq3[0]).ToString() + ((char)seq4[0]).ToString() + ((char)seq5[0]).ToString();
+                string smallSerial2 = ((char)seq1[1]).ToString() + ((char)seq2[1]).ToString() + ((char)seq3[1]).ToString() + ((char)seq4[1]).ToString() + ((char)seq5[1]).ToString();
+                string smallSerial3 = ((char)seq1[2]).ToString() + ((char)seq2[2]).ToString() + ((char)seq3[2]).ToString() + ((char)seq4[2]).ToString() + ((char)seq5[2]).ToString();
+                string smallSerial4 = ((char)seq1[3]).ToString() + ((char)seq2[3]).ToString() + ((char)seq3[3]).ToString() + ((char)seq4[3]).ToString() + ((char)seq5[3]).ToString();
+                string smallSerial5 = ((char)seq1[4]).ToString() + ((char)seq2[4]).ToString() + ((char)seq3[4]).ToString() + ((char)seq4[4]).ToString() + ((char)seq5[4]).ToString();
+
+                string serial = smallSerial1 + DIVIDER + smallSerial2 + DIVIDER + smallSerial3 + DIVIDER + smallSerial4 + DIVIDER + smallSerial5;
+                if (!collection.Contains(serial) && !doNotInclude.Contains(serial))
+                {
+                    collection.Add(serial);
+                    tbSerials.Text += serial + Environment.NewLine;
+                }
+            }
+        }
+
+        private int[] GetSequence()
+        {
+            int resultingsum = 0;
+            if (rbEisat.Checked)
+                resultingsum = RESULTINGSUM_EISAT;
+            else if (rbScreenshotOne.Checked)
+                resultingsum = RESULTINGSUM_SCRONEPRO;
+            else if (rbShutdown.Checked)
+                resultingsum = RESULTINGSUM_SHUT;
+            else if (rbVocabulary.Checked)
+                resultingsum = RESULTINGSUM_VOCAB;
+
+            int tempResult = 0, n0 = 0, n1 = 0, n2 = 0, n3 = 0, n4 = 0;
+            while (tempResult != resultingsum)
+            {
+                n0 = new Random(Guid.NewGuid().GetHashCode()).Next(48, 53);
+                n1 = new Random(Guid.NewGuid().GetHashCode()).Next(n0 + 2, 57);
+                n2 = new Random(Guid.NewGuid().GetHashCode()).Next(65, 76); //For "Next" numbers, refer to ASCII code table
+                n3 = new Random(Guid.NewGuid().GetHashCode()).Next(n2 + 2, 85);
+                n4 = new Random(Guid.NewGuid().GetHashCode()).Next(n3 + 2, 90);
+
+                //Small Serial has to match the following formula
+                tempResult = ((n0 + n2 + n4) - (n1 + n3));
+            }
+
+            return new int[] { n0, n1, n2, n3, n4 };
+        }
+    }
+}
