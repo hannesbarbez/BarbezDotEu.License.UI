79cfb03a8728b2c0cf79d02ae2f570c7dfb98d6b
Barbez.eu <hannesbarbez@hotmail.com>
2020-09-19 21:08:46 +0200

    Added .NET Core based version supporting using Task Parallel Library (TPL), still needs some fixes for UI blocks - almost there;

---
+++ BarbezDotEu.License.Generator.WinForms/BarbezDotEu.License.Generator.WinForms/FrmMain.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace BarbezDotEu.License.Generator.WinForms
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGenerateSerials = new System.Windows.Forms.Button();
+            this.rbScreenshotOne = new System.Windows.Forms.RadioButton();
+            this.rbEisat = new System.Windows.Forms.RadioButton();
+            this.rbVocabulary = new System.Windows.Forms.RadioButton();
+            this.rbShutdown = new System.Windows.Forms.RadioButton();
+            this.tbNumberOfSerials = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDoNotInclude = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbSerials = new System.Windows.Forms.TextBox();
+            this.pbProgress = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "# of license keys to generate:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "License keys to exclude:";
+            // 
+            // btnGenerateSerials
+            // 
+            this.btnGenerateSerials.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerateSerials.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnGenerateSerials.Enabled = false;
+            this.btnGenerateSerials.Location = new System.Drawing.Point(458, 12);
+            this.btnGenerateSerials.Name = "btnGenerateSerials";
+            this.btnGenerateSerials.Size = new System.Drawing.Size(125, 37);
+            this.btnGenerateSerials.TabIndex = 2;
+            this.btnGenerateSerials.Text = "&Generate keys";
+            this.btnGenerateSerials.UseVisualStyleBackColor = true;
+            this.btnGenerateSerials.Click += new System.EventHandler(this.BtnGenerateSerialNumber_Click);
+            // 
+            // rbScreenshotOne
+            // 
+            this.rbScreenshotOne.AutoSize = true;
+            this.rbScreenshotOne.Checked = true;
+            this.rbScreenshotOne.Location = new System.Drawing.Point(50, 49);
+            this.rbScreenshotOne.Name = "rbScreenshotOne";
+            this.rbScreenshotOne.Size = new System.Drawing.Size(129, 19);
+            this.rbScreenshotOne.TabIndex = 3;
+            this.rbScreenshotOne.TabStop = true;
+            this.rbScreenshotOne.Text = "&Screenshot One Pro";
+            this.rbScreenshotOne.UseVisualStyleBackColor = true;
+            // 
+            // rbEisat
+            // 
+            this.rbEisat.AutoSize = true;
+            this.rbEisat.Location = new System.Drawing.Point(185, 49);
+            this.rbEisat.Name = "rbEisat";
+            this.rbEisat.Size = new System.Drawing.Size(245, 19);
+            this.rbEisat.TabIndex = 4;
+            this.rbEisat.Text = "&Experience Index System Assessment Tool";
+            this.rbEisat.UseVisualStyleBackColor = true;
+            // 
+            // rbVocabulary
+            // 
+            this.rbVocabulary.AutoSize = true;
+            this.rbVocabulary.Location = new System.Drawing.Point(51, 75);
+            this.rbVocabulary.Name = "rbVocabulary";
+            this.rbVocabulary.Size = new System.Drawing.Size(99, 19);
+            this.rbVocabulary.TabIndex = 5;
+            this.rbVocabulary.Text = "&Vocabulary++";
+            this.rbVocabulary.UseVisualStyleBackColor = true;
+            // 
+            // rbShutdown
+            // 
+            this.rbShutdown.AutoSize = true;
+            this.rbShutdown.Location = new System.Drawing.Point(157, 75);
+            this.rbShutdown.Name = "rbShutdown";
+            this.rbShutdown.Size = new System.Drawing.Size(79, 19);
+            this.rbShutdown.TabIndex = 6;
+            this.rbShutdown.Text = "S&hutdown";
+            this.rbShutdown.UseVisualStyleBackColor = true;
+            // 
+            // tbNumberOfSerials
+            // 
+            this.tbNumberOfSerials.Location = new System.Drawing.Point(185, 13);
+            this.tbNumberOfSerials.Name = "tbNumberOfSerials";
+            this.tbNumberOfSerials.PlaceholderText = "max. 100000";
+            this.tbNumberOfSerials.Size = new System.Drawing.Size(80, 23);
+            this.tbNumberOfSerials.TabIndex = 1;
+            this.tbNumberOfSerials.TextChanged += new System.EventHandler(this.TbNumberOfSerials_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "App:";
+            // 
+            // tbDoNotInclude
+            // 
+            this.tbDoNotInclude.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbDoNotInclude.Location = new System.Drawing.Point(12, 127);
+            this.tbDoNotInclude.Multiline = true;
+            this.tbDoNotInclude.Name = "tbDoNotInclude";
+            this.tbDoNotInclude.PlaceholderText = "Type or paste any license keys to exclude from the list of generated license keys" +
+    ".";
+            this.tbDoNotInclude.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbDoNotInclude.Size = new System.Drawing.Size(571, 73);
+            this.tbDoNotInclude.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Generated license keys:";
+            // 
+            // tbSerials
+            // 
+            this.tbSerials.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSerials.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.tbSerials.Location = new System.Drawing.Point(12, 230);
+            this.tbSerials.Multiline = true;
+            this.tbSerials.Name = "tbSerials";
+            this.tbSerials.PlaceholderText = "Your generated license keys will appear here.";
+            this.tbSerials.ReadOnly = true;
+            this.tbSerials.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbSerials.Size = new System.Drawing.Size(571, 208);
+            this.tbSerials.TabIndex = 8;
+            // 
+            // pbProgress
+            // 
+            this.pbProgress.Location = new System.Drawing.Point(458, 55);
+            this.pbProgress.Name = "pbProgress";
+            this.pbProgress.Size = new System.Drawing.Size(125, 23);
+            this.pbProgress.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.pbProgress.TabIndex = 11;
+            this.pbProgress.Visible = false;
+            // 
+            // FrmMain
+            // 
+            this.AcceptButton = this.btnGenerateSerials;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 450);
+            this.Controls.Add(this.pbProgress);
+            this.Controls.Add(this.tbSerials);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbDoNotInclude);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbNumberOfSerials);
+            this.Controls.Add(this.rbShutdown);
+            this.Controls.Add(this.rbVocabulary);
+            this.Controls.Add(this.rbEisat);
+            this.Controls.Add(this.rbScreenshotOne);
+            this.Controls.Add(this.btnGenerateSerials);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "Barbez.eu License Key Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGenerateSerials;
+        private System.Windows.Forms.RadioButton rbScreenshotOne;
+        private System.Windows.Forms.RadioButton rbEisat;
+        private System.Windows.Forms.RadioButton rbVocabulary;
+        private System.Windows.Forms.RadioButton rbShutdown;
+        private System.Windows.Forms.TextBox tbNumberOfSerials;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDoNotInclude;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbSerials;
+        private System.Windows.Forms.ProgressBar pbProgress;
+    }
+}
+
---
+++ BarbezDotEu.License.Generator.WinForms/BarbezDotEu.License.Generator.WinForms/FrmMain.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarbezDotEu.License.Generator.WinForms
+{
+    public partial class FrmMain : Form
+    {
+        // 100000 = Still kind of acceptable before WinForms UI starts to give in.
+        private const uint MAXKEYS = 100000;
+        private const int RESULTINGSUM_SHUT = 70;
+        private const int RESULTINGSUM_SCRONEPRO = 68;
+        private const int RESULTINGSUM_EISAT = 66;
+        private const int RESULTINGSUM_VOCAB = 64;
+        private const string DIVIDER = "-";
+        private static uint numberOfKeys;
+        private static int resultingsum;
+        private HashSet<string> excluded;
+        private static ConcurrentBag<string> keys;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            tbNumberOfSerials.PlaceholderText = $"max. {MAXKEYS}";
+        }
+
+        private void TbNumberOfSerials_TextChanged(object sender, EventArgs e)
+        {
+            this.ValidateInputAndSetNumberOfKeys();
+        }
+
+        private async void BtnGenerateSerialNumber_Click(object sender, EventArgs e)
+        {
+            this.PrepareUiForNewKeys();
+            this.SetResultingSum();
+            var generatedKeys = await this.GenerateKeys(tbDoNotInclude.Lines);
+            this.PrepareUiWithNewKeys(generatedKeys.ToArray());
+        }
+
+        private void SetResultingSum()
+        {
+            if (rbEisat.Checked)
+                resultingsum = RESULTINGSUM_EISAT;
+            else if (rbScreenshotOne.Checked)
+                resultingsum = RESULTINGSUM_SCRONEPRO;
+            else if (rbShutdown.Checked)
+                resultingsum = RESULTINGSUM_SHUT;
+            else if (rbVocabulary.Checked)
+                resultingsum = RESULTINGSUM_VOCAB;
+        }
+
+        private void PrepareUiWithNewKeys(string[] generatedKeys)
+        {
+            tbSerials.Lines = generatedKeys;
+            this.ToggleUI();
+        }
+
+        private void PrepareUiForNewKeys()
+        {
+            this.ToggleUI();
+        }
+
+        private void ToggleUI()
+        {
+            var state = !btnGenerateSerials.Enabled;
+            btnGenerateSerials.Enabled = state;
+            tbNumberOfSerials.Enabled = state;
+            tbDoNotInclude.Enabled = state;
+            rbEisat.Enabled = state;
+            rbScreenshotOne.Enabled = state;
+            rbShutdown.Enabled = state;
+            rbVocabulary.Enabled = state;
+            pbProgress.Visible = !state;
+            if (pbProgress.Visible)
+                tbSerials.Clear();
+        }
+
+        public async Task<ConcurrentBag<string>> GenerateKeys(params string[] excludedKeys)
+        {
+            excluded = new HashSet<string>(excludedKeys.Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => x.Trim()));
+            keys = new ConcurrentBag<string>();
+            var tasks = CreateTasks();
+            for (int i = default; i < numberOfKeys; i++) tasks[i] = new Task(async () => await AddKeyToKeys());
+            Parallel.ForEach(tasks, task => task.Start());
+            await Task.WhenAll(tasks);
+            return keys;
+        }
+
+        private Task[] CreateTasks()
+        {
+            ConcurrentBag<Task> tasks = new ConcurrentBag<Task>();
+            Parallel.For(default, numberOfKeys, x =>
+            {
+                tasks.Add(new Task(async () => await AddKeyToKeys()));
+            });
+
+            return tasks.ToArray();
+        }
+
+        private Task AddKeyToKeys()
+        {
+            var key = this.GenerateKey();
+            if (!keys.Contains(key) && !excluded.Contains(key))
+            {
+                keys.Add(key);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private string GenerateKey()
+        {
+            var seq1 = GetSequence();
+            var seq2 = GetSequence();
+            var seq3 = GetSequence();
+            var seq4 = GetSequence();
+            var seq5 = GetSequence();
+
+            var pt1 = $"{((char)seq1[0])}{((char)seq2[0])}{((char)seq3[0])}{((char)seq4[0])}{((char)seq5[0])}";
+            var pt2 = $"{((char)seq1[1])}{((char)seq2[1])}{((char)seq3[1])}{((char)seq4[1])}{((char)seq5[1])}";
+            var pt3 = $"{((char)seq1[2])}{((char)seq2[2])}{((char)seq3[2])}{((char)seq4[2])}{((char)seq5[2])}";
+            var pt4 = $"{((char)seq1[3])}{((char)seq2[3])}{((char)seq3[3])}{((char)seq4[3])}{((char)seq5[3])}";
+            var pt5 = $"{((char)seq1[4])}{((char)seq2[4])}{((char)seq3[4])}{((char)seq4[4])}{((char)seq5[4])}";
+            var key = pt1 + DIVIDER + pt2 + DIVIDER + pt3 + DIVIDER + pt4 + DIVIDER + pt5;
+            return key;
+        }
+
+        private int[] GetSequence()
+        {
+            int tempResult = 0, n0 = 0, n1 = 0, n2 = 0, n3 = 0, n4 = 0;
+            while (tempResult != resultingsum)
+            {
+                // For "Next" parameters (numbers) exlanation, see ASCII code table.
+                n0 = new Random(Guid.NewGuid().GetHashCode()).Next(48, 53);
+                n1 = new Random(Guid.NewGuid().GetHashCode()).Next(n0 + 2, 57);
+                n2 = new Random(Guid.NewGuid().GetHashCode()).Next(65, 76);
+                n3 = new Random(Guid.NewGuid().GetHashCode()).Next(n2 + 2, 85);
+                n4 = new Random(Guid.NewGuid().GetHashCode()).Next(n3 + 2, 90);
+
+                // Sequence has to match the following formula.
+                tempResult = ((n0 + n2 + n4) - (n1 + n3));
+            }
+
+            return new int[] { n0, n1, n2, n3, n4 };
+        }
+
+        private void ValidateInputAndSetNumberOfKeys()
+        {
+            var isAcceptableInput = uint.TryParse(tbNumberOfSerials.Text, out uint value);
+            if (isAcceptableInput && value > uint.MinValue && value <= MAXKEYS) //uint.MaxValue)
+            {
+                numberOfKeys = value;
+                btnGenerateSerials.Enabled = true;
+            }
+            else
+            {
+                btnGenerateSerials.Enabled = false;
+            }
+        }
+    }
+}
